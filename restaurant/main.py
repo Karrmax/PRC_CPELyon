1afef76c21a64bff930173f6a40e73683d8128b8
Karrmax <jules.grivot.pelisson@gmail.com>
2025-06-13 12:00:49 +0200

    ajout affichage majordhome

--- restaurant/main.py
+++ restaurant/main.py
@@ -1,54 +1,85 @@
 import multiprocessing as mp
 import time
 import random
-
+from utils import *
 
 possible_commands = [chr(i) for i in range(65, 91)]
 
 def client_process(client_id, info, tempsMin=3, tempsMax=10):
-    print(f"Client {client_id} is starting.")
+    # print(f"Client {client_id} is starting.")
     while True:
         tempsAttente = random.randint(tempsMin, tempsMax)
         time.sleep(tempsAttente)
         
         commande = random.choice(possible_commands)
         # if info["clients"]["id" == client_id]["commande"] == []:
-        info["clients"]["id" == client_id]["commande"] += [commande]
+        info["clients"][client_id]["commande"] += manager.list((commande))
+        # print(client_id,info["clients"][client_id]["commande"],commande)
         # info["clients"]["id" == client_id]["etat"] = 1
         # print(f"Client {client_id} is placing an order: {commande}")
         commandesClients.put((client_id, commande))
 
 def server_process(server_id, info):
-    print(f"Server {server_id} is starting.")
+    # print(f"Server {server_id} is starting.")
     while True:
-        if commandesCuisiniers.empty():
+        if not commandesCuisiniers.empty():
+            client_id, commande = commandesCuisiniers.get()
+            info["servers"][server_id]["commande"] =  manager.list((client_id, commande))
+            info["servers"][server_id]["etat"] = 0
+            info["clients"][client_id]["commande"].remove(commande)  # Remove the command from the client's list
+            # info["clients"][client_id] = manager.dict(info["clients"][client_id])  # Ensure the client's state is updated in the manager
+            # print(f"Server {server_id} sending to client dish {client_id}: {commande}")
+            
+        elif not commandesClients.empty():
             client_id, commande = commandesClients.get()
             # print(f"Server {server_id} is processing order from Client {client_id}: {commande}")
-            info["servers"]["id" == server_id]["commande"] = (client_id, commande)
-            info["servers"]["id" == server_id]["etat"] = 1
+            info["servers"][server_id]["commande"] = manager.list((client_id, commande))
+            info["servers"][server_id]["etat"] = 1
+            info["servers"][server_id] = manager.dict(info["servers"][server_id])  # Ensure the server's state is updated in the manager
             time.sleep(random.uniform(0.5, 1.5))  # Simulate processing time
             commandesServeurs.put((server_id, client_id, commande))
-        else:
-            client_id, commande = commandesCuisiniers.get()
-            info["servers"]["id" == server_id]["commande"] = (client_id, commande)
-            info["servers"]["id" == server_id]["etat"] = 0
-            print(f"Server {server_id} sending to client dish {client_id}: {commande}")
-            
 
 def cuisinier_process(cuisinier_id, info):
-    print(f"Cuisinier {cuisinier_id} is starting.")
+    # print(f"Cuisinier {cuisinier_id} is starting.")
     while True:
-        server_id, client_id, commande = commandesServeurs.get()
-        # print(f"Cuisinier {cuisinier_id} is preparing order for Client {client_id} from Server {server_id}: {commande}")
-        info["cuisiniers"]["id" == cuisinier_id]["commande"] = (server_id, client_id, commande)
-        time.sleep(random.uniform(1, 2))
+        effacer_ecran()
+        
+        
+        server_id, client_id, commande = commandesServeurs.get()      # print(f"Cuisinier {cuisinier_id} is preparing order for Client {client_id} from Server {server_id}: {commande}")
+        info["cuisiniers"][cuisinier_id]["commande"] = manager.list((server_id, client_id, commande))
+        info["cuisiniers"][cuisinier_id] = manager.dict(info["cuisiniers"][cuisinier_id])
+        # print(info["cuisiniers"]["id" == cuisinier_id])
+        time.sleep(random.uniform(3, 5))
         commandesCuisiniers.put((client_id, commande))
+        info["cuisiniers"][cuisinier_id]["commande"] = manager.list()
 
 
 def majore_dHomme(infos):
-    print("Majore d'Homme is starting.")
+    # print("Majore d'Homme is starting.")
     while True:
-        print(infos)
+        commandesClientsList = [infos["clients"][i]["commande"] for i in range(len(infos["clients"]))]
+        commandesServeursList = [infos["servers"][i]["commande"] for i in range(len(infos["servers"]))]
+        commandesCuisiniersList = [infos["cuisiniers"][i]["commande"] for i in range(len(infos["cuisiniers"]))]
+        move_to(1,0) # pour effacer toute ma ligne
+        erase_line()
+        for i in range(len(infos["clients"])):
+            move_to(1+i,0) # pour effacer toute ma ligne
+            erase_line()
+            print(f"Client {i} - Commande: {infos['clients'][i]['commande']}")
+        for i in range(len(infos["servers"])):
+            move_to(len(infos["clients"]) +1+i,0) # pour effacer toute ma ligne
+            erase_line()
+            if infos["servers"][i]["etat"] == 0:
+                print(f"Server {i} - Commande: {infos['servers'][i]['commande']}")
+            else:
+                print(f"Server {i} - Apporte la commande: {infos['servers'][i]['commande']}")
+            # print(f"Server {i} - Commande: {infos['servers'][i]['commande']}, Etat: {infos['servers'][i]['etat']}")
+        for i in range(len(infos["cuisiniers"])):
+            move_to(len(infos["clients"]) + len(infos["servers"]) + 1+i, 0) # pour effacer toute ma ligne
+            erase_line()
+            print(f"Cuisinier {i} - Commande: {infos['cuisiniers'][i]['commande']}")
+        time.sleep(0.05)  # Update every 2 seconds
+        # print(infos)
         pass
     
 if __name__ == "__main__":
@@ -60,20 +91,21 @@ if __name__ == "__main__":
     commandesClients = mp.Queue() # commandes lorsque le client à choisis il ajoute sa commande dans cette queue
     commandesServeurs = mp.Queue() # le serveur prend en charge la commande du client et l'ajoute dans cette queue apres l'avoir traitée
     commandesCuisiniers = mp.Queue() # le cuisinier prend en charge la commande du serveur et l'ajoute dans cette queue apres l'avoir préparée
+    manager = mp.Manager()
+    allProcessInfo = manager.dict()
+    allProcessInfo["clients"] = manager.dict({i: {"commande": manager.list(), "etat": 0} for i in range(nbClients)})  # etat 0:cherche sa commande, 1:attend la commande
+    allProcessInfo["servers"] = manager.dict({i: {"etat": 0, "commande": manager.list()} for i in range(nbServers)})  # etat 0:attend une commande du client, 1: traite la commande
+    allProcessInfo["cuisiniers"] = manager.dict({i: {"etat": 0, "commande": manager.list()} for i in range(nbCuisinier)})  # etat 0:attend une commande du serveur, 1: prepare la commande
 
-
-    allProcessInfo = {
-        "clients": [{"id": i, "commande": []} for i in range(nbClients)], # etat 0:cherche sa commande, 1:attend la commande 
-        "servers": [{"id": i, "commande": []} for i in range(nbServers)], # etat 0:attend une commande du client, 1: traite la commande
-        "cuisiniers": [{"id": i, "etat": 0, "commande": []} for i in range(nbCuisinier)] # etat 0:attend une commande du serveur, 1: prepare la commande
-    }
+    effacer_ecran()
+    curseur_invisible()
+    #allProcessInfo["clients"][0]["commande"] = ["A", "B", "C"]  # Exemple de commande initiale pour le premier client
     
-    allProcessInfo = mp.Manager().dict(allProcessInfo)
 
     majore_dHommeProcess = mp.Process(target=majore_dHomme, args=(allProcessInfo,))
 
-    serverProcess = [mp.Process(target=server_process, args=(i, allProcessInfo)) for i in range(nbServers)]
     clientProcess = [mp.Process(target=client_process, args=(i, allProcessInfo)) for i in range(nbClients)]
+    serverProcess = [mp.Process(target=server_process, args=(i, allProcessInfo)) for i in range(nbServers)]
     cuisinierProcess = [mp.Process(target=cuisinier_process, args=(i, allProcessInfo)) for i in range(nbCuisinier)]
 
     majore_dHommeProcess.start()
---
+++ restaurant/utils.py
@@ -0,0 +1,43 @@
+# Quelques codes d''échappement
+CLEARSCR="\x1B[2J\x1B[;H" # Clear SCReen
+CLEAREOS = "\x1B[J" # Clear End Of Screen
+CLEARELN = "\x1B[2K" # Clear Entire LiNe
+CLEARCUP = "\x1B[1J" # Clear Curseur UP
+GOTOYX = "\x1B[%.2d;%.2dH" # (''H'' ou ''f'') : Goto at (y,x), voir le code
+DELAFCURSOR = "\x1B[K" # effacer après la position du curseur
+CRLF = "\r\n" # Retour à la ligne
+# VT100 : Actions sur le curseur
+CURSON = "\x1B[?25h" # Curseur visible
+CURSOFF = "\x1B[?25l" # Curseur invisible
+# VT100 : Actions sur les caractères affichables
+NORMAL = "\x1B[0m" # Normal
+BOLD = "\x1B[1m" # Gras
+UNDERLINE = "\x1B[4m" # Souligné
+# VT100 : Couleurs : "22" pour normal intensity
+CL_BLACK="\033[22;30m" # Noir. NE PAS UTILISER. On verra rien !!
+CL_RED="\033[22;31m" # Rouge
+CL_GREEN="\033[22;32m" # Vert
+CL_BROWN = "\033[22;33m" # Brun
+CL_BLUE="\033[22;34m" # Bleu
+CL_MAGENTA="\033[22;35m" # Magenta
+CL_CYAN="\033[22;36m" # Cyan
+CL_GRAY="\033[22;37m" # Gris
+# "01" pour quoi ? (bold ?)
+CL_DARKGRAY="\033[01;30m" # Gris foncé
+CL_LIGHTRED="\033[01;31m" # Rouge clair
+CL_LIGHTGREEN="\033[01;32m" # Vert clair
+CL_YELLOW="\033[01;33m" # Jaune
+CL_LIGHTBLU= "\033[01;34m" # Bleu clair
+CL_LIGHTMAGENTA="\033[01;35m" # Magenta clair
+CL_LIGHTCYAN="\033[01;36m" # Cyan clair
+CL_WHITE="\033[01;37m" # Blanc
+
+
+def effacer_ecran() : print(CLEARSCR,end='')
+def erase_line_from_beg_to_curs() : print("\033[1K",end='')
+def curseur_invisible() : print(CURSOFF,end='')
+def curseur_visible() : print(CURSON,end='')
+def move_to(lig, col) : print("\033[" + str(lig) + ";" + str(col) + "f",end='')
+def en_couleur(Coul) : print(Coul,end='')
+def en_rouge() : print(CL_RED,end='') # Un exemple !
+def erase_line() : print(CLEARELN,end='')
